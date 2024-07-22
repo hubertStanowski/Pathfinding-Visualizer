1cd3136510c4f2e86ae17e679c073a633a4b35f9
Hubert Stanowski <hubert.stanowski@gmail.com>
2024-07-22 18:54:52 +0200

    Add path drawing function

--- cleaner_redo/main.py
+++ cleaner_redo/main.py
@@ -98,8 +98,7 @@ def main():
                                         path = None
                                         print("PATH NOT FOUND")
                                     else:
-                                        pass
-                                        # draw_path(path)
+                                        draw_path(path)
                             elif label == "CLEAR":
                                 # Clear the graph (keep the barriers)
                                 graph.clear()
@@ -126,6 +125,35 @@ def main():
                         end = None
                     node.set_free()
                     node.draw(WINDOW, graph.gridlines)
+
+
+# Helper function for handling events within algorithms
+def run_checks():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            raise Exception(
+                "Exiting the program while executing an algorithm! (current settings will not be saved)")
+
+
+# Draw the path between start and end
+def draw_path(path, graph, animation_speed):
+    length = len(path)
+    prev = path[0]
+    for node in path[1:]:
+        run_checks()
+        if not prev.is_start():
+            prev.color = PATH_COLOR
+            prev.draw(update=True)
+        if not node.is_start() and not node.is_end():
+            node.color = YELLOW
+            node.draw(update=True)
+            prev = node
+        # Delay based on length relative to GRAPH_SIZE and base delay
+        delay = round(4 * graph.size / length * 6 *
+                      DELAYS[animation_speed][graph.size])
+        if delay > 80:
+            delay = 80
+        pygame.time.delay(delay)
 
 
 if __name__ == "__main__":
